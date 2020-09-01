bf668f2e2e0bce3cb18f6f06194d57bad8e9d46f
Daniel Moulson <dan24797@gmail.com>
2020-09-01 12:47:18 +0100

    comment out a config line

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SpreeDev
       end
     end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    #config.load_defaults 6.0
     config.autoload = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
